063e686ff955d0a5172fb736a79b73322c51b261
Saubhagya Tiwari <saubhagyatiwari19@gmail.com>
2026-06-15 15:34:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Portfolio Loaded Successfully");
+const text = "Software Developer | Machine Learning Enthusiast";
+
+let i = 0;
+
+function typeWriter() {
+
+    if(i < text.length){
+
+        document.getElementById("typing-text").innerHTML += text.charAt(i);
+
+        i++;
+
+        setTimeout(typeWriter, 70);
+    }
+}
+
+typeWriter();
